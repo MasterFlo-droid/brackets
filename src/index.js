014ffa2713a0535779724aa19c33c52e97651a3e
MasterFlo-droid <77565066+MasterFlo-droid@users.noreply.github.com>
2021-03-15 12:06:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracket = str;
+  for (let i = 0; i < bracket.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if(bracket[i] === bracketsConfig[j][0] && bracket[i + 1] === bracketsConfig[j][1]) {
+        let pairs = bracketsConfig[j].join('');
+        bracket = bracket.replace(pairs, '');
+        i -= 2;
+      }
+    }
+  }
+  return bracket == '';
 }
